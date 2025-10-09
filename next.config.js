3e3833574216a58c4749a57216aaa9d19be75684
mam4939 <81265817+mam4939@users.noreply.github.com>
2025-10-09 13:04:14 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: false }
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 };
-export default nextConfig;
+
+module.exports = nextConfig;
